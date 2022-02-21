bcfc4af4109d7af563f3e0afe86932ad41d4ebe4
Victor Wright <victor.wright@outlook.de>
2022-02-21 15:56:33 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
 import express from "express";
-import fs from "fs";
 import cors from "cors";
 import { dirname, join } from "path";
 import { fileURLToPath } from "url";
+import fs from "fs";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-import "./src/utils/config.cjs";
-// import { name as appName, version as appVer } from "./package.json";
-// const data = fs.readFileSync("./package.json");
-const packageJSON = JSON.parse(fs.readFileSync("./package.json"));
+// ------------ MY MODULES -----------
+import errorHandler from "./src/middlewares/errorHandler.js";
+// import errorHandler from join(__dirname, "/middlewares/errorHandler.js")
+// ------------ MY ROUTES -----------
+import authRouter from "./src/routes/router-auth.js";
+import baseRoute from "./src/routes/router-indexpage.js";
+import recipesRouter from "./src/routes/router1-recipes.js";
+import usersRouter from "./src/routes/router2-users.js";
+import categoriesRouter from "./src/routes/router3-categories.js";
+import ingredientsRouter from "./src/routes/router4-ingredients.js";
+import shareitemsRouter from "./src/routes/router5-shareitems.js";
+import plzRouter from "./src/routes/router6-plz.js";
 
+import "./src/utils/config.cjs";
+
+const packageJSON = JSON.parse(fs.readFileSync("./package.json"));
 const APPDATA = {
   PROJECT_NAME: packageJSON.name || "Node.js Project",
   PROJECT_VERSION: "v" + packageJSON.version || "0.1",
@@ -21,18 +32,6 @@ const APPDATA = {
   HOST: process.env.HOST || packageJSON.homepage || "http://127.0.0.1",
   PORT: process.env.PORT || 5000,
 };
-
-// ------------ MY MODULES -----------
-import errorHandler from "./src/middlewares/errorHandler.js";
-// ------------ MY ROUTES -----------
-import authRouter from "./src/routes/router-auth.js";
-import baseRoute from "./src/routes/router-indexpage.js";
-import recipesRouter from "./src/routes/router1-recipes.js";
-import usersRouter from "./src/routes/router2-users.js";
-import categoriesRouter from "./src/routes/router3-categories.js";
-import ingredientsRouter from "./src/routes/router4-ingredients.js";
-import shareitemsRouter from "./src/routes/router5-shareitems.js";
-import plzRouter from "./src/routes/router6-plz.js";
 
 const endPoints = {
   route0: ["/", "Info Page", baseRoute], //props issue!!
